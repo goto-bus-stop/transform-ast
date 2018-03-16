8909d9d8d459f8653e8abcfe725c9a3d89d2321f
Renée Kooi <renee@kooi.me>
2018-03-16 11:04:20 +0100

    add source map bench

--- bench/index.js
+++ bench/index.js
@@ -2,9 +2,18 @@ var bench = require('nanobench')
 var transformAst = require('../')
 var src = require('fs').readFileSync(require.resolve('acorn'), 'utf8')
 
-bench('transform acorn × 5', function (b) {
+bench('parse a big file × 5', function (b) {
   b.start()
   for (var i = 0; i < 5; i++)
     transformAst(src).toString()
+  b.end()
+})
+
+bench('dense source map for a big file × 5', function (b) {
+  b.start()
+  for (var i = 0; i < 5; i++)
+    transformAst(src, function (node) {
+      if (node.type === 'Identifier') node.edit.update(node.edit.source().toUpperCase())
+    }).toString({ map: true })
   b.end()
 })
