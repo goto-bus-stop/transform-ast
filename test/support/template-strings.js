3ddcece2938863657ff01cb40944e07d477c612a
Renée Kooi <renee@kooi.me>
2020-03-10 14:32:20 +0100

    Use Babel 6

--- test/support/template-strings.js
+++ test/support/template-strings.js
@@ -1,5 +1,5 @@
 if (!require('has-template-literals')()) {
-  require('@babel/register').default({
-    plugins: [require('@babel/plugin-transform-template-literals')]
+  require('babel-register').default({
+    plugins: [require('babel-plugin-transform-es2015-template-literals')]
   })
 }
